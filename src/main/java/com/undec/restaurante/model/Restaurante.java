ebd7a9f2d34b3bd98d0dbfdc3c2b5b22827cf114
hcmaza <hcmaza@yahoo.com.ar>
2019-08-27 10:43:31 -0300

    Update Restaurante.java

--- src/main/java/com/undec/restaurante/model/Restaurante.java
+++ src/main/java/com/undec/restaurante/model/Restaurante.java
@@ -1,16 +1,20 @@
 package com.undec.restaurante.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import javax.persistence.*;
 import java.util.Objects;
+import java.io.Serializable;
 
 @Entity
-public class Restaurante {
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Restaurante implements Serializable {
     private Integer id;
     private String nombre;
     private String direccion;
     private String descripcion;
     private String imagen;
     private String precio;
+    private static final long serialVersionUID = 1L;
 
     @Id
     @Column(name = "id")
