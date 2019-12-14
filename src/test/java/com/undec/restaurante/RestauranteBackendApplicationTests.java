e4cc183f2544504e172e1af9221229c6a17818fd
edgar <e.torrielli@gmail.com>
2019-12-14 12:07:38 -0300

    update

--- src/test/java/com/undec/restaurante/RestauranteBackendApplicationTests.java
+++ src/test/java/com/undec/restaurante/RestauranteBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RestauranteBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
